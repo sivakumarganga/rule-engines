d2fc09fafdd6c9b1d3c28e245d9cdb7e4074bb1d
Siva <sivakumarganga@gmail.com>
2024-09-24 22:53:42 +0530

    Initial commit

---
+++ src/RulesEngineX.MS/DiscountWorkflow.cs
@@ -0,0 +1,70 @@
+﻿using RulesEngine.Extensions;
+using RulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RulesEngineX.MS
+{
+    public class DiscountWorkflow
+    {
+        public async Task Invoke()
+        {
+            try
+            {
+                var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "Discount.json", SearchOption.AllDirectories);
+                if (files == null || files.Length == 0)
+                    throw new Exception("Rules not found.");
+
+                var fileData = File.ReadAllText(files[0]);
+                var workflows = JsonSerializer.Deserialize<List<Workflow>>(fileData);
+                //workflows[0].Rules.ToList()[0].RuleExpressionType = RuleExpressionType.LambdaExpression;
+
+
+                dynamic input1 = new { name = "hello", email = "abcd@xyz.com", creditHistory = "good", country = "canada", loyaltyFactor = 3, totalPurchasesToDate = 10000 };
+                dynamic input2 = new { totalOrders = 5, recurringItems = 2 };
+                dynamic input3 = new {noOfVisitsPerMonth= 10,percentageOfBuyingToVisit= 15};
+
+                var inputs = new dynamic[]
+                    {
+                    input1,
+                    input2,
+                    input3
+                    };
+                if (workflows is null)
+                    return;
+
+                var bre = new RulesEngine.RulesEngine(workflows.ToArray());
+
+
+                string discountOffered = "No discount offered.";
+
+                List<RuleResultTree> resultList = await bre.ExecuteAllRulesAsync("Discount", inputs);
+
+                resultList.OnSuccess((eventName) =>
+                {
+                    discountOffered = $"Discount offered is {eventName} % over MRP.";
+                });
+
+                resultList.OnFail(() =>
+                {
+                    discountOffered = "The user is not eligible for any discount.";
+                });
+
+                Console.WriteLine(discountOffered);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            await Task.FromResult(true);
+        }
+    }
+   
+}
---
+++ src/RulesEngineX.MS/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RulesEngineX.MS;
+
+Console.WriteLine("Hello, World!");
+DiscountWorkflow   discountWorkflow = new DiscountWorkflow();
+await discountWorkflow.Invoke();
+
+Console.Read();
